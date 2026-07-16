b260e26db37af0b03c278f0f7e5f999dd6edbdd8
mguenther <mgn@estino.de>
2026-07-16 15:24:40 +0200

    SelectionTable class

--- Source/OptiScan/Core/Database/SelectionTable.cpp
+++ Source/OptiScan/Core/Database/SelectionTable.cpp
@@ -1,5 +1,9 @@
 
 #include <OptiScan/Core/Database/SelectionTable.h>
+#include <fstream>
+
+using namespace nlohmann;
+using namespace std;
 
 namespace OptiScan::Core::Database
 {
@@ -8,4 +12,40 @@ namespace OptiScan::Core::Database
 	{
 	}
 
+	void SelectionTable::loadJsonFile(const filesystem::path & selectionTablePath)
+	{
+		this->_selectionTable.clear();
+		if (!filesystem::exists(selectionTablePath))
+		{
+			throw runtime_error("CanSelectionTable::loadFromFile: Selection table file '" + selectionTablePath.string() + "' does not exist.");
+		}
+		this->loadFromJson(selectionTablePath.string(), this->_jsonRootObject);
+	}
+
+	void SelectionTable::loadFromJson(const std::string & filePath, json & jsonRootObject)
+	{
+		ifstream file(filePath);
+		if (!file.is_open())
+		{
+			throw runtime_error("SystemConfig::loadConfigFile:Failed to open file: " + filePath);
+		}
+		try
+		{
+			file >> jsonRootObject;
+		}
+		catch (const json::parse_error & error)
+		{
+			throw runtime_error("SystemConfig::loadConfigFile: JSON Parse Error: " + string(error.what()));
+		}
+	}
+
+	void SelectionTable::clear()
+	{
+		this->_selectionTable.clear();
+	}
+
+	void CanSelectionTable::parse()
+	{
+
+	}
 }
--- Source/OptiScan/Core/Database/SelectionTable.h
+++ Source/OptiScan/Core/Database/SelectionTable.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <OptiScan/Core/Database/SelectedSignal.h>
+#include <Extern/Nlohmann/json.hpp>
+#include <filesystem>
 #include <vector>
 
 namespace OptiScan::Core::Database
@@ -10,9 +12,31 @@ namespace OptiScan::Core::Database
     {
     public:
         SelectionTable();
-    private:
+        virtual ~SelectionTable() = default;
+        /** @throw runtime_error if selection table file does not exist. */
+        void loadJsonFile(const std::filesystem::path & selectionTablePath);
+        /** */
+        virtual void parse() = 0;
+        /** */
+        void clear();
+    protected:
+        nlohmann::json _jsonRootObject;
         std::vector<SelectedSignal> _selectionTable;
+    private:
+        /** Open file and parse as nlohmann::json object.
+         * @throw runtime_error: If file failed to open.
+         * @throw runtime_error: If JSON Parse Error.
+         * @todo: Same method like in System config: Maby common json class with static method. */
+        static void loadFromJson(const std::string & filePath, nlohmann::json & jsonRootObject);
     
+    };
+
+    class CanSelectionTable : public SelectionTable
+    {
+    public:
+        CanSelectionTable() = default;
+        /**  */
+        virtual void parse() override;
     };
 
 }
