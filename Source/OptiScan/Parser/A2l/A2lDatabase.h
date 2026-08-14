63834da6ac179810b732fd6cf8ffb3e60d7d280c
mguenther <mgn@estino.de>
2026-08-14 12:41:47 +0200

    Build A2lDatabase

--- Source/OptiScan/Parser/A2l/A2lDatabase.h
+++ Source/OptiScan/Parser/A2l/A2lDatabase.h
@@ -1,13 +1,41 @@
 #pragma once
 
+#include <OptiScan/Parser/A2l/McdIdent.h>
+#include <OptiScan/Parser/A2l/McdModule.h>
 #include <string>
 #include <variant>
 #include <vector>
+#include <optional>
 
 namespace OptiScan::Parser::A2l
 {
+	struct McdHeader
+	{
+		std::string _comment;
+		McdIdent _projectNo;
+		std::string _version;
+	};
+
+	struct McdProject
+	{
+		std::optional<McdHeader> _header;
+		std::string _longIdentifier;
+		std::vector<McdModule> _modules;
+		McdIdent _name;
+	};
+
+	struct McdVersion
+	{
+		uint16_t _upgradeNo = 0;
+		uint16_t _versionNo = 0;
+	};
+
+
 	class A2lDatabase
 	{
-
+	public:
+		std::optional<McdVersion> _a2mlVersion;
+		McdVersion _mcdVersion;
+		McdProject _project;
 	};
 }
